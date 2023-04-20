606e5c1e417c8f7fd0d3569417dddc7f1a4fa9d2
NoahSmith04 <124379601+NoahSmith04@users.noreply.github.com>
2023-04-20 14:53:22 +0000

    done

---
+++ js/script.js
@@ -0,0 +1,50 @@
+// Copyright (c) 2023 Noah Smith All rights reserved
+//
+// Created by: Noah Smith
+// Created on: Apr 2023
+// This file contains the JS functions for index.html
+
+
+"use strict";
+
+/**
+ * This function determines what movie the user is allowed to watch based on their age
+ */
+function UserAge() {
+  //get age of user
+  let result = "";
+  let age = parseInt(document.getElementById("user-age").value);
+  
+  // Tells user to enter a positive age
+  if (age < 0) {
+    result = "Enter a valid age!";
+  }
+
+  // Tells user that they can watch R rated movies
+  else if (age > 17) {
+    result = "You can watch R rated movies by yourself.";
+  }
+
+  // Tells user that they can watch PG-13 movies
+  else if (age > 12) {
+    result = "You can watch PG-13 rated movies by yourself.";
+  }
+
+  // Tells user that they can watch PG or G rated movies
+  else if (age >= 5 ) {
+    result = "You can watch PG or G rated movies by yourself.";
+  }
+
+  // Tells user that they cannot watch a movie alone
+  else if (age < 5) {
+    result = "You should not watch a movie alone.";
+  }
+
+  // Tells user to enter their age instead of leaving it blank
+  else {
+    result = "Enter an age!";
+  }
+  
+  // Displays what movie they can watch back to the user
+  document.getElementById("result").innerHTML = result;
+}
